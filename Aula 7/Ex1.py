8e732a2f31f494224d1a839b1731b7e446e4145b
unknown <afre.luz10@gmail.com>
2025-08-25 16:20:26 -0300

    inicio do estudo da aula 7

---
+++ Aula 7/Ex1.py
@@ -0,0 +1,4 @@
+estoque = ['camiseta','calça','meia','jaqueta']
+estoque.pop(1)
+estoque.insert(1,'bone')
+print(estoque)
---
+++ Aula 7/Ex2.py
@@ -0,0 +1,4 @@
+alunos = ['Bruno','Ana','Carlos','Denise','Felipe']
+alunos.sort(reverse=True)
+
+print(f'Alunos visto {alunos[0]} e {alunos[4]} ')
